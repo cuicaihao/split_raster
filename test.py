5ac93b63a6a3624b0bf7d0761a38a5181fdd01b1
Chris (Caihao) Cui <cuicaihao@users.noreply.github.com>
2021-08-11 15:17:18 +1000

    Add PyTest and Update workflow

--- test.py
+++ test.py
@@ -2,90 +2,84 @@
 from splitraster import geo
 from splitraster import io
 
-# %%
-input_image_path = "./data/raw/RGB.png"
-gt_image_path = "./data/raw/GT.png"
-
-save_path = "./data/processed/Rand/RGB"
-save_path_gt = "./data/processed/Rand/GT"
-
-n = io.random_crop_image(input_image_path, save_path,  gt_image_path, save_path_gt,
-                         crop_size=256, crop_number=20, img_ext='.jpg', label_ext='.png', overwrite=True)
-
-print(f"{n} sample paris of {input_image_path, gt_image_path} are added at {save_path, save_path_gt}")
-
 
 # %% Example A:
-# Step 1: set input image file path
-input_image_path = "./data/raw/RGB.png"
-gt_image_path = "./data/raw/GT.png"
+def test_rgb_gt_slide_window():
+    # Step 1: set input image file path
+    input_image_path = "./data/raw/RGB.png"
+    gt_image_path = "./data/raw/GT.png"
 
-# Step 2: prepare output directory and spliting configuration
-input_save_path = "../data/processed/RGB"
-gt_save_path = "../data/processed/GT"
+    # Step 2: prepare output directory and spliting configuration
+    input_save_path = "../data/processed/RGB"
+    gt_save_path = "../data/processed/GT"
 
-crop_size = 256
-repetition_rate = 0
-overwrite = False
+    crop_size = 256
+    repetition_rate = 0
+    overwrite = False
 
-# step 3: split the RGB images
-n = io.split_image(input_image_path, input_save_path, crop_size,
-                   repetition_rate=repetition_rate, overwrite=overwrite)
-print(f"{n} tiles sample of {input_image_path} are added at {input_save_path}")
+    # step 3: split the RGB images
+    n = io.split_image(input_image_path, input_save_path, crop_size,
+                       repetition_rate=repetition_rate, overwrite=overwrite)
+    print(f"{n} tiles sample of {input_image_path} are added at {input_save_path}")
 
-# step 4: split the GT images
-n = io.split_image(gt_image_path, gt_save_path, crop_size,
-                   repetition_rate=repetition_rate, overwrite=overwrite)
-print(f"{n} tiles sample of {gt_image_path} are added at {gt_save_path}")
+    # step 4: split the GT images
+    n = io.split_image(gt_image_path, gt_save_path, crop_size,
+                       repetition_rate=repetition_rate, overwrite=overwrite)
+    print(f"{n} tiles sample of {gt_image_path} are added at {gt_save_path}")
 
-# Step 5: Use the RGB and GT folders for your deep learning model.
-
-# %% Example AA: General image seg dataset: Random Sampling Method
-input_image_path = "./data/raw/RGB.png"
-gt_image_path = "./data/raw/GT.png"
-
-input_save_path = "./data/processed/Rand/RGB"
-gt_save_path = "./data/processed/Rand/GT"
-
-n = io.random_crop_image(input_image_path, input_save_path,  gt_image_path, gt_save_path,
-                         crop_size=256, crop_number=20, img_ext='.png', label_ext='.png', overwrite=True)
-
-print(f"{n} sample paris of {input_image_path, gt_image_path} are added at {input_save_path, gt_save_path}.")
+    # Step 5: Use the RGB and GT folders for your deep learning model.
 
 
-# %% Example B: Remote Sensing image seg dataset - Slide Window Method
+# %% Example B
+def test_rgb_gt_random_crop():
+    input_image_path = "./data/raw/RGB.png"
+    gt_image_path = "./data/raw/GT.png"
 
-input_tif_image_path = "./data/raw/TIF/RGB5k.tif"
-gt_tif_image_path = "./data/raw/TIF/GT5k.tif"
+    save_path = "./data/processed/Rand/RGB"
+    save_path_gt = "./data/processed/Rand/GT"
 
-input_save_image_path = "./data/processed/RGB_TIF"
-gt_save_image_path = "./data/processed/GT_TIF"
+    n = io.random_crop_image(input_image_path, save_path,  gt_image_path, save_path_gt,
+                             crop_size=256, crop_number=20, img_ext='.png', label_ext='.png', overwrite=True)
 
-crop_size = 500
-repetition_rate = 0
-overwrite = True
+    print(f"{n} sample paris of {input_image_path, gt_image_path} are added at {save_path, save_path_gt}")
 
-n = geo.split_image(input_tif_image_path, input_save_image_path,
-                    crop_size, repetition_rate, overwrite)
 
-print(f"{n} tiles sample of {input_tif_image_path} are added at {input_save_image_path}")
+# %% Example C
+def test_tif_slide_window():
+    input_tif_image_path = "./data/raw/TIF/RGB5k.tif"
+    gt_tif_image_path = "./data/raw/TIF/GT5k.tif"
 
-n = geo.split_image(gt_tif_image_path, gt_save_image_path,
-                    crop_size, repetition_rate, overwrite)
+    input_save_image_path = "./data/processed/RGB_TIF"
+    gt_save_image_path = "./data/processed/GT_TIF"
 
-print(f"{n} tiles sample of {gt_tif_image_path} are added at {gt_save_image_path}")
+    crop_size = 500
+    repetition_rate = 0
+    overwrite = True
 
-# %% Example B: Remote Sensing image seg dataset - Random Sampling Method
-input_tif_image_path = "./data/raw/TIF/RGB5k.tif"
-gt_tif_image_path = "./data/raw/TIF/GT5k.tif"
+    n = geo.split_image(input_tif_image_path, input_save_image_path,
+                        crop_size, repetition_rate, overwrite)
 
-input_save_image_path = "./data/processed/Rand/RGB_TIF"
-gt_save_image_path = "./data/processed/Rand/GT_TIF"
+    print(f"{n} tiles sample of {input_tif_image_path} are added at {input_save_image_path}")
 
-n = geo.random_crop_image(input_tif_image_path, input_save_image_path,  gt_tif_image_path, gt_save_image_path,
-                          crop_size=500, crop_number=20, overwrite=True)
+    n = geo.split_image(gt_tif_image_path, gt_save_image_path,
+                        crop_size, repetition_rate, overwrite)
 
-print(f"{n} sample paris of {input_tif_image_path, gt_tif_image_path} are added at {input_save_image_path, gt_save_image_path}.")
+    print(f"{n} tiles sample of {gt_tif_image_path} are added at {gt_save_image_path}")
+
+
+# %% Example D
+def test_tif_random_sample():
+    input_tif_image_path = "./data/raw/TIF/RGB5k.tif"
+    gt_tif_image_path = "./data/raw/TIF/GT5k.tif"
+
+    input_save_image_path = "./data/processed/Rand/RGB_TIF"
+    gt_save_image_path = "./data/processed/Rand/GT_TIF"
+
+    n = geo.random_crop_image(input_tif_image_path, input_save_image_path,  gt_tif_image_path, gt_save_image_path,
+                              crop_size=500, crop_number=20, overwrite=True)
+
+    print(f"{n} sample paris of {input_tif_image_path, gt_tif_image_path} are added at {input_save_image_path, gt_save_image_path}.")
+
 
 # %% Show END
 print("PASS")
